e708dc8719718b0298b5a6cd062cc5581c6829d2
Jahnavi30392 <140414533+Jahnavi30392@users.noreply.github.com>
2024-12-11 14:52:56 +0530

    wtf is this feedbackService.java

--- src/main/java/com/example/demo/feedbackService.java
+++ src/main/java/com/example/demo/feedbackService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins="*")
 @RestController
 public class feedbackService {
 
